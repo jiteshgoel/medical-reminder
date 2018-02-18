c979fff88e045bfb5d91f0cbe320fce2f1cf878d
Sukrit-Mehta <sukritmehta1996@gmail.com>
2018-02-18 06:25:24 +0530

    first one

--- app/src/main/java/com/example/medicinereminder/medicinereminder/Activities/MainActivity.java
+++ app/src/main/java/com/example/medicinereminder/medicinereminder/Activities/MainActivity.java
@@ -16,7 +16,6 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
@@ -244,12 +243,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
